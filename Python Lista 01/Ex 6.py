9eda98f4fdec388e64da1ed6effdba8bc2b22b2c
Débora Yamao <89618716+DeboraYamao@users.noreply.github.com>
2021-09-17 21:48:33 -0300

    Create Ex 6.py

---
+++ Python Lista 01/Ex 6.py
@@ -0,0 +1,10 @@
+'''
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+'''
+pi = 3.14
+
+raio = float(input("Digite a medida do raio: "))
+
+area = pi * (raio**2)
+
+print("A aréa do círculo:",area)
