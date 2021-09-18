41f3dcf8209c158d138fe807766d79345ec51806
Débora Yamao <89618716+DeboraYamao@users.noreply.github.com>
2021-09-17 21:55:39 -0300

    Create Ex 07.py

---
+++ Python Lista 01/Ex 07.py
@@ -0,0 +1,11 @@
+'''
+Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+'''
+base = 3.14
+
+base = float(input("Digite a medida da base: "))
+altura = float(input("Digite a medida da altura: "))
+
+area = base * altura
+
+print(f'A área do quadrado: {area}\nO dobro da área {area*2}')
